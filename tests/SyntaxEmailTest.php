92417d7f9ee4ef12f8290d15c41588e88893e5e7
marcinlacina <marcin.lacina@zslt.klucznet.pl>
2016-04-17 21:50:15 +0200

    refactor of SyntaxEmailTest

--- tests/SyntaxEmailTest.php
+++ tests/SyntaxEmailTest.php
@@ -2,22 +2,44 @@
 
 class SyntaxEmailTest extends PHPUnit_Framework_TestCase
 {
-   public function testCreateEmailWithOneRecipientByConstruct()
-   {
-       $testedAddress = "test@recipient.com";
-       $message = new \SyntaxErro\SmtpBundle\SyntaxEmail("Whatever.", $testedAddress);
-       $this->assertTrue(is_array($message->getTo()), "Message recipients is not array.");
-       $this->assertContains($testedAddress, $message->getTo(), "Message recipients not contain address passed by construct.");
-   }
-
-    public function testCreateEmailWithMultipleRecipientsByConstruct()
+    /**
+     * Test adding recipients by construct.
+     *
+     * @param $testedAddresses
+     * @dataProvider testCreateEmailWithRecipientsByConstructProvider
+     */
+    public function testCreateEmailWithRecipientsByConstruct($testedAddresses)
     {
-        $testedAddresses = ["test@recipient.com", "other@address.com"];
         $message = new \SyntaxErro\SmtpBundle\SyntaxEmail("Whatever.", $testedAddresses);
         $this->assertTrue(is_array($message->getTo()), "Message recipients is not array.");
-        $this->assertEquals($testedAddresses, $message->getTo(), "Message recipients not contain addresses passed by construct.");
+
+        if(is_array($testedAddresses)) {
+            foreach($testedAddresses as $testedAddress) {
+                $this->assertContains($testedAddress, $message->getTo(), sprintf("Message recipients not contain added '%s' address.", $testedAddress));
+            }
+        } else {
+            $this->assertContains($testedAddresses, $message->getTo(), "Message recipients not contain '$testedAddresses' address passed by construct.");
+        }
+
     }
 
+    /**
+     * Add one recipient as string
+     * or multiple recipients as array of strings.
+     *
+     * @return array
+     */
+    public function testCreateEmailWithRecipientsByConstructProvider()
+    {
+        return [
+            ["test@address.com"],
+            [["test@address.com", "other@address.com"]]
+        ];
+    }
+
+    /**
+     * Test manual adding of recipients in loop.
+     */
     public function testCreateEmailWithManualAddingRecipients()
     {
         $testedAddresses = ["test@recipient.com", "other@address.com"];
